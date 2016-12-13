95ce891b7d2e4d3020d35f4afd34e94beae0af8e
jssmile <jssmile19921112@gmail.com>
2016-12-13 09:15:29 +0000

    Load the image of C310 and calibrate it

--- calibration.py
+++ calibration.py
@@ -1,25 +1,26 @@
 import numpy as np
 import cv2
+import glob
 
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points
-objp = np.zeros((6*7, 3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((6*8, 3), np.float32)
+objp[:,:2] = np.mgrid[0:8,0:6].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-images = glob.glob('*.jpg')
+images = glob.glob('caca/*.jpg')
 
 for fname in images:
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
     # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+    ret, corners = cv2.findChessboardCorners(gray, (8,6),None)
 
     # If found, add object points, image points (after refining them)
     if ret == True:
@@ -27,10 +28,11 @@ for fname in images:
 
         cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
         imgpoints.append(corners)
-
         # Draw and display the corners
-        cv2.drawChessboardCorners(img, (7,6), corners2,ret)
-        cv2.imshow('img',img)
+        cv2.drawChessboardCorners(img, (8,6), corners, ret)
+        ret, mtx, dist, rvecs,tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+        print(mtx)
+        # Draw and display the corners
+        cv2.imshow('img', img)
         cv2.waitKey(500)
-
 cv2.destroyAllWindows()
