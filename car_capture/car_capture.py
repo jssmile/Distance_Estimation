9a7a6118863eea6368a07c25ce377cc9f52f6e97
jssmile <jssmile19921112@gmail.com>
2017-02-21 10:55:47 +0800

    Car detection with opencv and harr-feature

---
+++ car_capture/car_capture.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+count = 0
+pos = 'car_capture/'
+pic_format = '.jpg'
+#define the codec
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('test.avi', fourcc, 30.0, (640, 480))
+def Snap(img):
+    global count
+    NAME = pos + str(count) + pic_format
+    cv2.imwrite(NAME, img)
+    print("Saved ", NAME )
+    count += 1
+
+
+while (True):
+    _, frame = cap.read()
+    cv2.imshow("frame", frame)
+    out.write(frame)
+    key = cv2.waitKey(1)
+    if key & 0xFF == ord('q'):
+        print("quit")
+        break
+    elif key & 0xFF == ord('s'):
+        Snap(frame)
+cap.release()
+out.release()
+cv2.destroyAllWindows() 
