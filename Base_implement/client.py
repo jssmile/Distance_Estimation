fd85afd126bc9ff9a0308ebbe966a4404fa84da7
jssmile <jssmile19921112@gmail.com>
2017-03-03 11:46:44 +0800

    update

--- Base_implement/client.py
+++ Base_implement/client.py
@@ -2,7 +2,7 @@ import socket
 import cv2
 import numpy
 
-TCP_IP = '140.116.164.7'
+TCP_IP = '140.116.164.8'
 TCP_PORT = 5001
 
 sock = socket.socket()
@@ -20,25 +20,23 @@ def recvall(sock, count):
     return buf
 
 while (True):
-	ret, frame = capture.read()
+	_, frame = capture.read()
 
 	encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),30]
 	_, imgencode = cv2.imencode('.jpg', frame, encode_param)
-	data = numpy.array(imgencode)
-	stringData = data.tostring()
+	data_send = numpy.array(imgencode)
+	stringData_send = data_send.tostring()
 
-	sock.send(str(len(stringData)).ljust(16));
-	sock.send(stringData);
+	sock.send(str(len(stringData_send)).ljust(16));
+	sock.send(stringData_send);
 
 	length = recvall(sock,16)
 	stringData_recv = recvall(sock, int(length))
 	data_recv = numpy.fromstring(stringData_recv, dtype='uint8')
 
-	decimg_recv = cv2.imdecode(data_recv,1)
-	cv2.imshow('SERVER', decimg_recv)
+	frame_recv = cv2.imdecode(data_recv, 1)
+	cv2.imshow("ssd", frame_recv)
 	cv2.waitKey(1)
 
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
 sock.close()
-cv2.destroyAllWindows() 
+cv2.destroyAllWindows()
