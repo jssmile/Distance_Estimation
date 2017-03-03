2d9101e25af5eaafd67836efb6f33d9bccbd254f
jssmile <jssmile19921112@gmail.com>
2017-03-03 11:41:09 +0800

    can communicate with server

--- Base_implement/client.py
+++ Base_implement/client.py
@@ -2,27 +2,41 @@ import socket
 import cv2
 import numpy
 
-TCP_IP = 'localhost'
+TCP_IP = '140.116.164.8'
 TCP_PORT = 5001
 
 sock = socket.socket()
 sock.connect((TCP_IP, TCP_PORT))
 
 capture = cv2.VideoCapture(0)
+
+def recvall(sock, count):
+    buf = b''
+    while count:
+        newbuf = sock.recv(count)
+        if not newbuf: return None
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
+
 while (True):
-	ret, frame = capture.read()
+	_, frame = capture.read()
 
-	encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
-	result, imgencode = cv2.imencode('.jpg', frame, encode_param)
-	data = numpy.array(imgencode)
-	stringData = data.tostring()
+	encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),30]
+	_, imgencode = cv2.imencode('.jpg', frame, encode_param)
+	data_send = numpy.array(imgencode)
+	stringData_send = data_send.tostring()
 
-	sock.send( str(len(stringData)).ljust(16));
-	sock.send( stringData );
+	sock.send(str(len(stringData_send)).ljust(16));
+	sock.send(stringData_send);
 
-	decimg=cv2.imdecode(data,1)
-	cv2.imshow('CLIENT',decimg)
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+	length = recvall(sock,16)
+	stringData_recv = recvall(sock, int(length))
+	data_recv = numpy.fromstring(stringData_recv, dtype='uint8')
+
+	frame_recv = cv2.imdecode(data_recv, 1)
+	cv2.imshow("ssd", frame_recv)
+	cv2.waitKey(1)
+
 sock.close()
 cv2.destroyAllWindows() 
