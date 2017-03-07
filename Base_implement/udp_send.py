17f0b31cc8f9c3158ce96f02ef9b56ecc5ba35da
jssmile <jssmile19921112@gmail.com>
2017-03-07 15:05:35 +0800

    update

--- Base_implement/udp_send.py
+++ Base_implement/udp_send.py
@@ -1,14 +1,41 @@
 import numpy as np
 import cv2
 import socket
+import datetime
 
 UDP_IP = "140.116.164.8"
 UDP_PORT = 5005
+cnt = 0
+fps = 0
 
 cap = cv2.VideoCapture(0)
 
 while(True):
     ret, frame = cap.read()
+    cnt = cnt + 1
+    if cnt == 1:
+        start = datetime.datetime.now()
+    cv2.putText(frame,
+                    "fps" + str(fps),
+                    (0, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (150,0,255),
+                    2)
+    if cnt == 10:
+    	end = datetime.datetime.now()
+    	period = end - start
+    	period = period.total_seconds()
+    	fps = 10 / period
+    	print(str(fps))
+    	cnt = 0
+    	cv2.putText(frame,
+                    "fps" + str(fps),
+                    (0, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (150,0,255),
+                    2)
 
     cv2.imshow('frame',frame)
 
@@ -20,7 +47,7 @@ while(True):
 
 
 
-    for i in xrange(20):
+    for i in xrange(16):
 
         sock.sendto (s[i*46080:(i+1)*46080],(UDP_IP, UDP_PORT))
 
