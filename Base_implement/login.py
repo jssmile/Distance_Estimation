74d94135659a908bdf84d523eab90fd9462d5d41
jssmile <jssmile19921112@gmail.com>
2017-03-08 23:51:14 +0800

    login with the ip address and port

--- Base_implement/login.py
+++ Base_implement/login.py
@@ -1,45 +1,35 @@
-import Tkinter
-import TKinter.messagebox as tm
+from Tkinter import *
+class App:
+  def __init__(self, master):
+    frame = Frame(master)
+    frame.pack()
 
+    self.label_1 = Label(frame, text="Username")
+    self.label_2 = Label(frame, text="Password")
+    self.label_1.grid(row=0, sticky=E)
+    self.label_2.grid(row=1, sticky=E)
 
-class LoginFrame(Frame):
-    def __init__(self, master):
-        super().__init__(master)
+    self.entry_1 = Entry(frame)
+    self.entry_2 = Entry(frame)
+    self.entry_1.grid(row=0, column=1)
+    self.entry_2.grid(row=1, column=1)
 
-        self.label_1 = Label(self, text="Username")
-        self.label_2 = Label(self, text="Password")
-
-        self.entry_1 = Entry(self)
-        self.entry_2 = Entry(self, show="*")
-
-        self.label_1.grid(row=0, sticky=E)
-        self.label_2.grid(row=1, sticky=E)
-        self.entry_1.grid(row=0, column=1)
-        self.entry_2.grid(row=1, column=1)
-
-        self.checkbox = Checkbutton(self, text="Keep me logged in")
-        self.checkbox.grid(columnspan=2)
-
-        self.logbtn = Button(self, text="Login", command = self._login_btn_clickked)
-        self.logbtn.grid(columnspan=2)
-
-        self.pack()
-
-    def _login_btn_clickked(self):
-        #print("Clicked")
-        username = self.entry_1.get()
-        password = self.entry_2.get()
-
-        #print(username, password)
-
-        if username == "john" and password == "password":
-            tm.showinfo("Login info", "Welcome John")
-        else:
-            tm.showerror("Login error", "Incorrect username")
-
-
-
+    self.Connect_btn = Button(frame, text ="Connect", command = self.write_slogan)
+    self.Connect_btn.grid(row=3, column=0)
+    self.Cancel_btn = Button(frame, 
+                         text="Exit", fg="red",
+                         command=quit)
+    self.Cancel_btn.grid(row=3, column = 1)
+  def write_slogan(self):
+    ip_add = self.entry_1.get()
+    port = self.entry_2.get()
+    print(ip_add, port)
+    root.quit()
 
 root = Tk()
-lf = LoginFrame(root)
+app = App(root)
 root.mainloop()
+
+ip_add = app.entry_1.get()
+port = app.entry_2.get()
+print(ip_add, port)
