a2191701ac05bfbf1802fb963066ce03c05d84d7
jssmile <jssmile19921112@gmail.com>
2017-03-08 11:16:48 +0800

    Add the login example

---
+++ Base_implement/login.py
@@ -0,0 +1,45 @@
+import Tkinter
+import TKinter.messagebox as tm
+
+
+class LoginFrame(Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.label_1 = Label(self, text="Username")
+        self.label_2 = Label(self, text="Password")
+
+        self.entry_1 = Entry(self)
+        self.entry_2 = Entry(self, show="*")
+
+        self.label_1.grid(row=0, sticky=E)
+        self.label_2.grid(row=1, sticky=E)
+        self.entry_1.grid(row=0, column=1)
+        self.entry_2.grid(row=1, column=1)
+
+        self.checkbox = Checkbutton(self, text="Keep me logged in")
+        self.checkbox.grid(columnspan=2)
+
+        self.logbtn = Button(self, text="Login", command = self._login_btn_clickked)
+        self.logbtn.grid(columnspan=2)
+
+        self.pack()
+
+    def _login_btn_clickked(self):
+        #print("Clicked")
+        username = self.entry_1.get()
+        password = self.entry_2.get()
+
+        #print(username, password)
+
+        if username == "john" and password == "password":
+            tm.showinfo("Login info", "Welcome John")
+        else:
+            tm.showerror("Login error", "Incorrect username")
+
+
+
+
+root = Tk()
+lf = LoginFrame(root)
+root.mainloop()
