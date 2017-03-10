a6d56c0b8dfb8477e6149bb10f19dbabaff499eb
jssmile <jssmile19921112@gmail.com>
2017-03-10 20:53:14 +0800

    modulize the code

--- ssd_TCP_client.py
+++ ssd_TCP_client.py
@@ -7,21 +7,8 @@ import numpy
 TCP_IP = None
 TCP_PORT = None
 
-def Nothing():
-	pass
-
-# Receive the image package
-def recvall(sock, count):
-    buf = b''
-    while count:
-        newbuf = sock.recv(count)
-        if not newbuf: return None
-        buf += newbuf
-        count -= len(newbuf)
-    return buf
-
 # The login interface
-class App:
+class Login:
   def __init__(self, master):
     frame = Frame(master)
     frame.pack()
@@ -44,6 +31,7 @@ class App:
                          text="Exit", fg="red",
                          command=quit)
     self.Cancel_btn.grid(row=3, column = 1)
+  
   def write_slogan(self):
     global TCP_IP, TCP_PORT
     TCP_IP = self.entry_1.get()
@@ -53,40 +41,60 @@ class App:
 
 root = Tk()
 root.wm_title("Client")
-app = App(root)
+app = Login(root)
 root.mainloop()
 
-sock = socket.socket()
-sock.connect((TCP_IP, TCP_PORT))
+def Nothing():
+	pass
 
-capture = cv2.VideoCapture(0)
+# Receive the image package
+def recvall(sock, count):
+    buf = b''
+    while count:
+        newbuf = sock.recv(count)
+        if not newbuf: return None
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
 
-cv2.namedWindow("ssd", cv2.WND_PROP_FULLSCREEN)
-cv2.setWindowProperty("ssd", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-cv2.createTrackbar('Quality', 'ssd', 50, 100, Nothing)
+def connection():
+	sock = socket.socket()
+	sock.connect((TCP_IP, TCP_PORT))
+	return sock
 
-while (True):
-	_, frame = capture.read()
+def main():
+	while (True):
+		_, frame = capture.read()
 
-	quality = cv2.getTrackbarPos('Quality', 'ssd')
-	encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), quality]
-	_, imgencode = cv2.imencode('.jpg', frame, encode_param)
-	data_send = numpy.array(imgencode)
-	stringData_send = data_send.tostring()
+		quality = cv2.getTrackbarPos('Quality', 'ssd')
+		encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), quality]
+		_, imgencode = cv2.imencode('.jpg', frame, encode_param)
+		data_send = numpy.array(imgencode)
+		stringData_send = data_send.tostring()
 
-	sock.send(str(len(stringData_send)).ljust(16));
-	sock.send(stringData_send);
+		sock.send(str(len(stringData_send)).ljust(16));
+		sock.send(stringData_send);
 
-	length = recvall(sock,16)
-	stringData_recv = recvall(sock, int(length))
-	data_recv = numpy.fromstring(stringData_recv, dtype='uint8')
+		length = recvall(sock,16)
+		stringData_recv = recvall(sock, int(length))
+		data_recv = numpy.fromstring(stringData_recv, dtype='uint8')
 
-	frame_recv = cv2.imdecode(data_recv, 1)
+		frame_recv = cv2.imdecode(data_recv, 1)
 	
-	cv2.imshow("ssd", frame_recv)
-	cv2.waitKey(1)
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		os.exit()
+		cv2.imshow("ssd", frame_recv)
+		cv2.waitKey(1)
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			os.exit()
 
-sock.close()
-cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+	capture = cv2.VideoCapture(0)
+
+	cv2.namedWindow("ssd", cv2.WND_PROP_FULLSCREEN)
+	cv2.setWindowProperty("ssd", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+	cv2.createTrackbar('Quality', 'ssd', 50, 100, Nothing)
+	
+	sock = connection()
+	main()
+	sock.close()
+	cv2.destroyAllWindows()
