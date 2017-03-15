e7af6bc6f22fc67c25e239745b45727518fc1a86
jssmile <jssmile19921112@gmail.com>
2017-03-15 17:02:35 +0800

    use for only local detection, NOT for socket !

---
+++ ssd_local.py
@@ -0,0 +1,198 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import datetime
+
+plt.rcParams['figure.figsize'] = (10, 10)
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+# Make sure that caffe is on the python path:
+caffe_root = '/home/jssmile/Distance_Estimation/caffe'  # this file is expected to be in {caffe_root}/examples
+import os
+os.chdir(caffe_root)
+import sys
+sys.path.insert(0, 'python')
+
+import caffe
+caffe.set_device(0)
+caffe.set_mode_gpu()
+
+from google.protobuf import text_format
+from caffe.proto import caffe_pb2
+
+def get_labelname(labelmap, labels):
+    num_labels = len(labelmap.item)
+    labelnames = []
+    if type(labels) is not list:
+        labels = [labels]
+    for label in labels:
+        found = False
+        for i in xrange(0, num_labels):
+            if label == labelmap.item[i].label:
+                found = True
+                labelnames.append(labelmap.item[i].display_name)
+                break
+        assert found == True
+    return labelnames
+
+# focal length
+focal_length = 816
+
+# car's real width(cm)
+car_width = 3.5
+
+# image size
+width = 640
+height = 480
+scale = 2
+
+# frames counter
+cnt = 0
+counted = False
+
+# Open the webcam
+cap = cv2.VideoCapture(0)
+
+#define the codec
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('test.avi', fourcc, 9.5, (640, 480))
+
+# load PASCAL VOC labels
+labelmap_file = 'data/VOC0712/labelmap_voc.prototxt'
+file = open(labelmap_file, 'r')
+labelmap = caffe_pb2.LabelMap()
+text_format.Merge(str(file.read()), labelmap)
+
+model_def = 'models/VGGNet/VOC0712/SSD_300x300/deploy.prototxt'
+model_weights = 'models/VGGNet/VOC0712/SSD_300x300/VGG_VOC0712_SSD_300x300_iter_120000.caffemodel'
+
+net = caffe.Net(model_def,      # defines the structure of the model
+                model_weights,  # contains the trained weights
+                caffe.TEST)     # use test mode (e.g., don't perform dropout)
+
+# input preprocessing: 'data' is the name of the input blob == net.inputs[0]
+transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+transformer.set_transpose('data', (2, 0, 1))
+transformer.set_mean('data', np.array([104,117,123])) # mean pixel
+transformer.set_raw_scale('data', 255)  # the reference model operates on images in [0,255] range instead of [0,1]
+transformer.set_channel_swap('data', (2,1,0))  # the reference model has channels in BGR order instead of RGB
+
+# set net to batch size of 1
+image_resize = 300
+net.blobs['data'].reshape(1,3,image_resize,image_resize)
+
+while (True):
+    _, frame = cap.read()
+    cnt = cnt + 1
+    
+    if cnt == 1:
+        start = datetime.datetime.now()
+
+    image = frame.astype(np.float32)/255
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    image = image[...,::-1]
+    transformed_image = transformer.preprocess('data', image)
+    net.blobs['data'].data[...] = transformed_image
+
+    # Forward pass.
+    detections = net.forward()['detection_out']
+
+    # Parse the outputs.
+    det_label = detections[0,0,:,1]
+    det_conf = detections[0,0,:,2]
+    det_xmin = detections[0,0,:,3]
+    det_ymin = detections[0,0,:,4]
+    det_xmax = detections[0,0,:,5]
+    det_ymax = detections[0,0,:,6]
+
+    # Get detections with confidence higher than 0.6.
+    top_indices = [i for i, conf in enumerate(det_conf) if conf >= 0.6]
+
+    top_conf = det_conf[top_indices]
+    top_label_indices = det_label[top_indices].tolist()
+    top_labels = get_labelname(labelmap, top_label_indices)
+    top_xmin = det_xmin[top_indices]
+    top_ymin = det_ymin[top_indices]
+    top_xmax = det_xmax[top_indices]
+    top_ymax = det_ymax[top_indices]
+    colors = plt.cm.hsv(np.linspace(0, 1, 21)).tolist()
+
+    for i in xrange(top_conf.shape[0]):
+        xmin = int(round(top_xmin[i] * image.shape[1]))
+        ymin = int(round(top_ymin[i] * image.shape[0]))
+        xmax = int(round(top_xmax[i] * image.shape[1]))
+        ymax = int(round(top_ymax[i] * image.shape[0]))
+        score = top_conf[i]
+        label = int(top_label_indices[i])
+        label_name = top_labels[i]
+
+        if label_name == 'car':
+            img_car_width = xmax-xmin
+            distance = (focal_length * car_width)/img_car_width
+            #color = colors[label].astype(np.float8)*255
+            cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (150,0,255), 2)
+            cv2.putText(frame,
+                        label_name,
+                        (xmin, ymin),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        1,
+                        (150,0,255),
+                        2)
+            cv2.putText(frame,
+                        "%.2fcm" % distance,
+                        (xmax, ymax),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        1,
+                        (0, 255, 0),
+                        2)
+        else :
+            color = colors[label]
+            cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (150,0,255), 2)
+            cv2.putText(frame,
+                    label_name,
+                    (xmin, ymin),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (150,0,255),
+                    2)
+
+    if counted:
+        cv2.putText(frame,
+                    "fps : " + str(fps),
+                    (0, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (150,0,255),
+                    2)
+
+    cv2.imshow('frame', frame)
+
+    # count 60 frames and calculated the frames per seconds(fps) 
+    if cnt == 60:
+        end = datetime.datetime.now()
+        period = end - start
+        fps = 60 / (period.total_seconds())
+        cv2.putText(frame,
+                    str(fps),
+                    (0, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (150,0,255),
+                    2)
+        cnt = 0
+        counted = True
+    
+    # Save the images as a video file
+    out.write(np.uint8(frame))
+
+    # Press 'q' to stop the program
+    key = cv2.waitKey(1)
+    if key & 0xFF == ord('q'):
+        print("quit")
+        break
+
+# Clear the buffer
+cap.release()
+out.release()
+cv2.destroyAllWindows()
