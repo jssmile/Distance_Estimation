f7e166ea2f17a5926e850d6cf5c2f281e90691c4
jssmile <jssmile19921112@gmail.com>
2017-03-16 16:37:12 +0800

    Completet the comments

--- Base_implement/car_capture/car_capture.py
+++ Base_implement/car_capture/car_capture.py
@@ -1,3 +1,14 @@
+'''
+ * Copyright 2017 Distance Measurement, EE, NCKU. All rights reserved. 
+ * File : car_capture.py 
+ * User : Syuan Jhao 
+ * Date : 2017/3/16 
+ * Version : 1.0
+ * OS : Ubuntu Mate 16.04 LTS
+ * Tools : Python 2.7 + Opencv 3.2.0
+ * Introduction : Capture the video and press 's' to save the video frame.
+'''
+
 import cv2
 import numpy as np
 
@@ -5,9 +16,12 @@ cap = cv2.VideoCapture(0)
 count = 0
 pos = 'car_capture/'
 pic_format = '.jpg'
-#define the codec
+
+#define the video codec
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('test.avi', fourcc, 30.0, (640, 480))
+
+# Take the snap shot
 def Snap(img):
     global count
     NAME = str(count) + pic_format
