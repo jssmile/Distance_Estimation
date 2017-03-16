3bf0530a4c9d02e80c7e226925bb1a4f59e76796
jssmile <jssmile19921112@gmail.com>
2017-03-16 16:19:36 +0800

    Complete the comment

--- Base_implement/server.py
+++ Base_implement/server.py
@@ -1,7 +1,19 @@
+'''
+ * Copyright 2017 Distance Measurement, EE, NCKU. All rights reserved. 
+ * File : server.py 
+ * User : Syuan Jhao 
+ * Date : 2017/3/16 
+ * Version : 1.0
+ * OS : Ubuntu Mate 16.04 LTS
+ * Tools : Python 2.7 + Opencv 3.2.0
+ * Introduction : Receive the camera frames from client and send them back.
+'''
+
 import socket
 import cv2
 import numpy
 
+# Receive the image data
 def recvall(sock, count):
     buf = b''
     while count:
@@ -11,6 +23,7 @@ def recvall(sock, count):
         count -= len(newbuf)
     return buf
 
+# IP and port number. must same as server's
 TCP_IP = '140.116.164.7'
 TCP_PORT = 5001
 
@@ -21,10 +34,11 @@ server.listen(True)
 conn, addr = server.accept()
 
 while (True):
+    # Receive the data
 	length = recvall(conn,16)
 	stringData = recvall(conn, int(length))
 	data = numpy.fromstring(stringData, dtype='uint8')
-
+    
 	decimg=cv2.imdecode(data,1)
 	cv2.imshow('SERVER', decimg)
 	cv2.waitKey(1)
