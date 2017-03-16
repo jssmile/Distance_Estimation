b4a2a6c6a9835442cef5d5f165ffce0ed49a01c5
jssmile <jssmile19921112@gmail.com>
2017-03-16 16:39:57 +0800

    Complete the comments

--- Base_implement/car_capture/car_detection.py
+++ Base_implement/car_capture/car_detection.py
@@ -1,3 +1,15 @@
+'''
+ * Copyright 2017 Distance Measurement, EE, NCKU. All rights reserved. 
+ * File : car_detection.py 
+ * User : Syuan Jhao 
+ * Date : 2017/3/16 
+ * Version : 1.0
+ * OS : Ubuntu Mate 16.04 LTS
+ * Tools : Python 2.7 + Opencv 3.2.0
+ * Introduction : Detect the vehicle by Haar-feature
+ * Guide : python car_detection.py /path/to/cascadefile.xml /path/to/image
+'''
+
 import cv2
 import numpy as np
 import sys
@@ -12,8 +24,8 @@ car_width = 170.0
 
 #car pic detection
 #   Get User supplied values
-imgPath = sys.argv[2]
 cascPath  = sys.argv[1]
+imgPath = sys.argv[2]
 
 img = cv2.imread(imgPath)
 car_cascade = cv2.CascadeClassifier(cascPath)
@@ -33,34 +45,3 @@ cv2.imshow('car detection', img)
 cv2.imwrite("cars_detection.jpg", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
-"""
-#Video capture
-
-cascPath  = sys.argv[1]
-vidPath = sys.argv[2]
-#	Capture video from camera
-cap = cv2.VideoCapture(vidPath)
-car_Cascade = cv2.CascadeClassifier(cascPath)
-while(True):
-	ret, img = cap.read()
-	if(type(img) == type(None)):
-		break
-
-	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	cars = car_Cascade.detectMultiScale(gray, 1.2, 1)
-	print("Found {0} cars!".format(len(cars)))
-	
-	for (x, y, w, h) in cars:
-		cv2.rectangle(img, (x,y), (x+w, y+h), (0, 0, 255), 2)
-		distance = (focal_length * car_width)/w
-		print(distance, "cm")
-		cv2.putText(img, "%.2fcm" % distance, (x+w, y+h), cv2.FONT_HERSHEY_SIMPLEX,2.0, (0, 255, 0), 2)
-	cv2.imshow('car_detection', img)
-
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-"""
